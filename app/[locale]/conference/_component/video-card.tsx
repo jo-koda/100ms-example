9126af68446df4350c7255584922790cbf612de1
koda-mhel <mhel.tagpuno@kodakollectiv.com>
2024-12-13 14:37:53 +0800

    Feat: video ui

--- app/[locale]/conference/_component/video-card.tsx
+++ app/[locale]/conference/_component/video-card.tsx
@@ -1,19 +1,25 @@
-'use client';
+'use client'
 
-import { HMSPeer, useVideo } from '@100mslive/react-sdk';
-import { Card, CardHeader, CardBody } from '@nextui-org/react';
+import { HMSPeer, useVideo } from '@100mslive/react-sdk'
+import { Card, CardHeader, CardBody } from '@nextui-org/react'
 
 export default function VideoCard({ peer }: { peer: HMSPeer }) {
   const { videoRef } = useVideo({
     trackId: peer.videoTrack,
-  });
+  })
 
   return (
-    <div>
-      <video ref={videoRef} autoPlay muted playsInline />
-      <div className="peer-name">
+    <div className='relative max-h-[700px] min-h-[300px] min-w-[300px] max-w-screen-lg'>
+      <video
+        className='h-auto w-full'
+        ref={videoRef}
+        autoPlay
+        muted
+        playsInline
+      />
+      <div className='peer-name absolute left-2 top-2 rounded-full bg-black/70 px-4 py-2 text-sm text-white'>
         {peer.name} {peer.isLocal ? '(You)' : ''}
       </div>
     </div>
-  );
+  )
 }
