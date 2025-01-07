49f8665afed6c2ac9050d91aeffacf40ff6fe3a1
ylosorio12 <jo.ylosorio@kodakollectiv.com>
2025-01-07 13:43:57 +0800

    feat: example

--- app/[locale]/conference/_sections/conference-room.tsx
+++ app/[locale]/conference/_sections/conference-room.tsx
@@ -1,37 +1,38 @@
-'use client'
+'use client';
 
-import { selectPeers, useHMSStore } from '@100mslive/react-sdk'
-import { Card, CardHeader, CardBody } from '@nextui-org/react'
-import VideoCard from '../_component/video-card'
-import { redirect } from 'next/navigation'
-import SubVideoCard from '../_component/sub-video-card'
+import { selectPeers, useHMSStore } from '@100mslive/react-sdk';
+import { Card, CardHeader, CardBody } from '@nextui-org/react';
+import VideoCard from '../_component/video-card';
+import { redirect } from 'next/navigation';
+import SubVideoCard from '../_component/sub-video-card';
 
 export default function ConferenceRoom() {
-  const peers = useHMSStore(selectPeers)
-  console.log(peers)
+  const peers = useHMSStore(selectPeers);
   if (peers.length != 0) {
     return (
-      <div className='flex w-full flex-col items-center gap-2 overflow-auto'>
-        {peers.map((peer) => (
-          <VideoCard key={peer.id} peer={peer} />
-        ))}
-        <div className='max-w-screen-lg overflow-x-auto'>
-          <div className='flex min-w-[150%] gap-4 border'>
-            {Array(6)
-              .fill(null)
-              .flatMap(() =>
-                peers.map((peer) => (
+      <div className="flex w-full flex-col items-center gap-2 overflow-auto">
+        {peers.map((peer, index) => {
+          if (index == 0) {
+            return <VideoCard key={peer.id} peer={peer} />;
+          }
+        })}
+        <div className="min-h-10 max-w-screen-lg overflow-x-auto">
+          <div className="flex min-w-[150%] gap-4 border">
+            {peers.map((peer, index) => {
+              if (index != 0) {
+                return (
                   <SubVideoCard
                     key={`${peer.id}-${Math.random()}`}
                     peer={peer}
                   />
-                )),
-              )}
+                );
+              }
+            })}
           </div>
         </div>
       </div>
-    )
+    );
   } else {
-    redirect('/en/rooms')
+    redirect('/en/rooms');
   }
 }
